9d79cc86cd4dfa49f972846244a8c939dae57d38
jwhite-usgs <jwhite@usgs.gov>
2017-08-08 11:01:18 -0500

    added wel and rch pars to freyberg_pp

--- activities/freyberg_pp.py
+++ activities/freyberg_pp.py
@@ -14,8 +14,13 @@ WORKING_DIR = 'freyberg_' + PREFIX
 BASE_MODEL_DIR = os.path.join("..","models","Freyberg","Freyberg_Truth")
 BASE_MODEL_NAM = "freyberg.truth.nam"
 MODEL_NAM = "freyberg.nam"
-PST_NAME = WORKING_DIR+".pst"
-
+#PST_NAME = WORKING_DIR+".pst"
+PST_NAME = "freyberg.pst"
+NUM_SLAVES = 10
+NUM_STEPS_RESPSURF = 50
+EXE_PREF = ''
+if "window" not in platform.platform().lower():
+    EXE_PREF = "./"
 
 def setup_model():
 
@@ -35,6 +40,7 @@ def setup_model():
     m.name = MODEL_NAM.split(".")[0]
     m.lpf.hk = m.lpf.hk.array.mean()
     m.lpf.hk[0].format.free = True
+
     wel_data_sp1 = m.wel.stress_period_data[0]
     #wel_data_sp1["flux"] = np.ceil(wel_data_sp1["flux"],order=)
     wel_data_sp1["flux"] = [round(f,-2) for f in wel_data_sp1["flux"]]
@@ -43,6 +49,7 @@ def setup_model():
 
     r = np.round(m.rch.rech[0].array.mean(),5)
     m.rch.rech[0] = r
+    m.rch.rech[0].format.free = True
     m.external_path = '.'
     #m.oc.chedfm = "(20f16.6)"
     #output_idx = m.output_fnames.index("freyberg.hds")
@@ -62,12 +69,19 @@ def setup_model():
 def setup_pest():
 
     os.chdir(WORKING_DIR)
+
+    # setup hk pilot point parameters
     m = flopy.modflow.Modflow.load(MODEL_NAM,check=False)
     pp_space = 4
     df_pp = pyemu.gw_utils.setup_pilotpoints_grid(ml=m,prefix_dict={0:["hk"]},
                                           every_n_cell=pp_space)
     pp_file = "hkpp.dat"
+
+
+    # setup list file water budget obs
     df_wb = pyemu.gw_utils.setup_mflist_budget_obs(m.name+".list")
+
+    # setup potential water budget obs
     perlen = pd.to_datetime(m._start_datetime) + pd.to_timedelta(np.cumsum(m.dis.perlen.array),unit='d')
     #obsnme = []
     print(perlen,m.dis.perlen.array)
@@ -90,6 +104,7 @@ def setup_pest():
     df_po = pd.read_csv("freyberg.hds.dat.obf",delim_whitespace=True,names=["obsnme","obsval"])
     df_po.index = df_po.obsnme
 
+    # set particle travel time obs
     with open("freyberg.travel.ins",'w') as f:
         f.write("pif ~\n")
         f.write("l1 w !travel_time!\n")
@@ -97,20 +112,75 @@ def setup_pest():
     forecast_names = list(df_wb.loc[df_wb.obsnme.apply(lambda x: "riv" in x and "flx" in x),"obsnme"])
     forecast_names.append("travel_time")
 
+    # setup wel parameters - history and future
+    wel_fmt = {"l":lambda x: '{0:10.0f}'.format(x)}
+    wel_fmt["r"] = wel_fmt['l']
+    wel_fmt["c"] = wel_fmt['l']
+    wel_fmt["flux"] = lambda x: '{0:15.6E}'.format(x)
+
+    wel_files = ["WEL_0001.dat","WEL_0002.dat"]
+    w_dfs = []
+    for iwel,wel_file in enumerate(wel_files):
+
+        df_wel = pd.read_csv(wel_file,delim_whitespace=True,header=None,names=["l","r","c","flux"])
+        df_wel.loc[:,"parnme"] = df_wel.apply(lambda x: "w{0}_r{1:02.0f}_c{2:02.0f}".format(iwel,x.r,x.c),axis=1)
+        df_wel.loc[:,"tpl_str"] = df_wel.parnme.apply(lambda x: "~  {0}   ~".format(x))
+        f_tpl = open(wel_file+".temp.tpl",'w')
+        f_tpl.write('ptf ~\n')
+        f_tpl.write("        "+df_wel.loc[:,["l","r","c","tpl_str"]].to_string(index=False,header=False,formatters=wel_fmt))
+        f_tpl.close()
+        w_dfs.append(df_wel.loc[:,["parnme","tpl_str"]])
+        df_wel.loc[:,"flux"] = 1.0
+        with open(wel_file+".temp",'w') as f:
+            f.write(df_wel.loc[:,["l","r","c","flux"]].to_string(index=False,header=False,formatters=wel_fmt))
+        shutil.copy2(wel_file,wel_file+".bak")
+    df_wel = pd.concat(w_dfs)
+    df_wel.index = df_wel.parnme
+
+
+    # setup rch parameters - history and future
+    f_in = open(MODEL_NAM.replace(".nam",".rch"),'r')
+    f_tpl = open(MODEL_NAM.replace(".nam",".rch.tpl"),'w')
+    f_tpl.write("ptf ~\n")
+    r_count = 0
+    for line in f_in:
+        raw = line.strip().split()
+        if "open" in line.lower() and r_count < 2:
+            raw[2] = "~  rch_{0}   ~".format(r_count)
+            r_count += 1
+        line = ' '.join(raw)
+        f_tpl.write(line+'\n')
+    f_in.close()
+    f_tpl.close()
+
+    # build lists of tpl, in, ins, and out files
     tpl_files = [f for f in os.listdir(".") if f.endswith(".tpl")]
     in_files = [f.replace(".tpl","") for f in tpl_files]
 
     ins_files = [f for f in os.listdir(".") if f.endswith(".ins")]
     out_files = [f.replace(".ins","") for f in ins_files]
 
+    # build a control file
     pst = pyemu.pst_utils.pst_from_io_files(tpl_files,in_files,
                                             ins_files,out_files)
+
+    # set some observation attribues
     obs = pst.observation_data
     obs.loc[df_wb.obsnme,"obgnme"] = df_wb.obgnme
     obs.loc[df_wb.obsnme,"weight"] = 0.0
     obs.loc[forecast_names,"weight"] = 0.0
     obs.loc[df_po.obsnme,"obsval"] = df_po.obsval
     obs.loc[df_po.obsnme,"weight"] = 0.0
+
+    # set some parameter attribs
+    par = pst.parameter_data
+    par.loc[df_pp.parnme,"parval1"] = 5.0
+    par.loc[df_pp.parnme,"parlbnd"] = 0.5
+    par.loc[df_pp.parnme,"parubnd"] = 50.0
+    par.loc[df_wel.parnme,"parval1"] = 1.0
+    par.loc[df_wel.parnme,"parubnd"] = 1.25
+    par.loc[df_wel.parnme,"parlbnd"] = 0.75
+
     pst.model_command = ["python forward_run.py"]
     pst.pestpp_options["forecasts"] = ','.join(forecast_names)
     pst.write(PST_NAME)
@@ -132,12 +202,24 @@ def setup_pest():
         #f.write("import sys\nsys.path.append('..')\n")
         #f.write("import {0}\n".format(fname))
         #f.write("{0}.forward_run()\n".format(fname))
-        f.write("import os\nimport numpy as np\nimport pyemu\nimport flopy\n")
+        f.write("import os\nimport shutil\nimport pandas as pd\nimport numpy as np\nimport pyemu\nimport flopy\n")
         #f.write("pp_files = [f for f in os.listdir('.') if f.endswith('pp.dat')]\n")
         #f.write("for pp_file in pp_files:\n")
         #f.write("    pyemu.gw_utils.fac2real(pp_file,factors_file=pp_file+'.fac',out_file='hk_layer_1.ref')\n")
+        f.write("wel_files = ['WEL_0001.dat','WEL_0002.dat']\n")
+        f.write("names=['l','r','c','flux']\n")
+        f.write("wel_fmt = {'l':lambda x: '{0:10.0f}'.format(x)}\n")
+        f.write("wel_fmt['r'] = wel_fmt['l']\n")
+        f.write("wel_fmt['c'] = wel_fmt['l']\n")
+        f.write("wel_fmt['flux'] = lambda x: '{0:15.6E}'.format(x)\n")
+        f.write("for wel_file in wel_files:\n")
+        f.write("    df_w = pd.read_csv(wel_file+'.bak',header=None,names=names,delim_whitespace=True)\n")
+        f.write("    df_t = pd.read_csv(wel_file+'.temp',header=None,names=names,delim_whitespace=True)\n")
+        f.write("    df_t.loc[:,'flux'] *= df_w.flux\n")
+        f.write("    with open(wel_file,'w') as f:\n")
+        f.write("        f.write('        '+df_t.loc[:,names].to_string(index=None,header=None,formatters=wel_fmt)+'\\n')\n")
+        f.write("shutil.copy2('WEL_0002.dat','WEL_0003.dat')\n")
         f.write("pyemu.gw_utils.fac2real('hkpp.dat',factors_file='hkpp.dat.fac',out_file='hk_layer_1.ref')\n")
-
         f.write("pyemu.helpers.run('mf2005 {0} >_mf2005.stdout')\n".format(MODEL_NAM))
         f.write("pyemu.helpers.run('mp6 freyberg.mpsim >_mp6.stdout')\n")
         f.write("pyemu.gw_utils.apply_mflist_budget_obs('{0}')\n".format(MODEL_NAM.replace(".nam",".list")))
@@ -172,7 +254,8 @@ def run_pe():
     #os.system("pestpp {0} 1>_pestpp_stdout 2>_pestpp_stderr".format(PST_NAME))
     #pyemu.helpers.run("pestpp {0} 1>_pestpp_stdout 2>_pestpp_stderr".format(PST_NAME))
     os.chdir(WORKING_DIR)
-    pyemu.helpers.run("pestpp {0}".format(PST_NAME))
+    #pyemu.helpers.run("pestpp {0}".format(PST_NAME))
+    pyemu.helpers.start_slaves('.','pestpp',PST_NAME,num_slaves=NUM_SLAVES,master_dir='.')
     os.chdir("..")
 
 def run_fosm():
@@ -188,8 +271,11 @@ def run_dataworth():
     jco = os.path.join(WORKING_DIR,PST_NAME.replace('.pst','.jcb'))
     assert os.path.exists(jco),"jco not found:{0}".format(jco)
     sc = pyemu.Schur(jco=jco)
-    df = sc.get_added_obs_importance(base_obslist=[],reset_zero_weight=True)
-    df.to_csv(jco+".addobs.csv")
+    df_ao = sc.get_added_obs_importance(base_obslist=[],reset_zero_weight=True)
+    df_ao.to_csv(jco+".addobs.csv")
+
+    df_pc = sc.get_par_contribution()
+    df_pc.to_csv(jco+".parcontrib.csv")
 
 
 def run_mc():
@@ -198,7 +284,8 @@ def run_mc():
     mc.draw(1000)
     mc.parensemble.to_csv(os.path.join(WORKING_DIR,"sweep_in.csv"))
     os.chdir(WORKING_DIR)
-    pyemu.helpers.run("sweep {0}".format(PST_NAME))
+    #pyemu.helpers.run("sweep {0}".format(PST_NAME))
+    pyemu.helpers.start_slaves('.','sweep',PST_NAME,num_slaves=NUM_SLAVES,master_dir='.')
     os.chdir("..")
     df_obs = pd.read_csv(os.path.join(WORKING_DIR,"sweep_out.csv"))
 
@@ -214,16 +301,36 @@ def run_gsa():
     pyemu.helpers.run("gsa {0}".format(PST_NAME))
     os.chdir("..")
 
-def run_respsurf():
-    pass
+def run_respsurf(par_names=None):
+    pst = pyemu.Pst(os.path.join(WORKING_DIR,PST_NAME))
+    par = pst.parameter_data
+    icount = 0
+    if par_names is None:
+        parnme1 = par.parnme[0]
+        parnme2 = par.parnme[1]
+    else:
+        parnme1 = par_names[0]
+        parnme2 = par_names[1]
+    p1 = np.linspace(par.loc[parnme1,"parlbnd"],par.loc[parnme1,"parubnd"],NUM_STEPS_RESPSURF).tolist()
+    p2 = np.linspace(par.loc[parnme2,"parlbnd"],par.loc[parnme2,"parubnd"],NUM_STEPS_RESPSURF).tolist()
+    p1_vals,p2_vals = [],[]
+    for p in p1:
+        p1_vals.extend(list(np.zeros(NUM_STEPS_RESPSURF)+p))
+        p2_vals.extend(p2)
+    df = pd.DataFrame({parnme1:p1_vals,parnme2:p2_vals})
+    df.to_csv(os.path.join(WORKING_DIR,"sweep_in.csv"))
+
+
+
 
 def run_ies():
     pass
 
 
 if __name__ == "__main__":
-    #setup_model()
-    #setup_pest()
+    setup_model()
+    setup_pest()
     #run_pe()
-    run_fosm()
-    run_dataworth()
+    #run_fosm()
+    #run_dataworth()
+    #run_respsurf()
