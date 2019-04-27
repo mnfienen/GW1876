18603494926a60bedd69cb7f4af59c5762c5f154
Jeremy White <jtwhite1000@gmail.com>
2019-04-27 19:03:45 +1200

    checkin before trying run notebooks script

---
+++ activities_2day_mfm/notebooks/run_notebooks.sh
@@ -0,0 +1,5 @@
+jupyter nbconvert --to notebook --inplace setup_pest_interface.ipynb
+jupyter nbconvert --to notebook --inplace prior_montecarlo.ipynb
+jupyter nbconvert --to notebook --inplace pestpp-glm.ipynb
+jupyter nbconvert --to notebook --inplace pestpp-ies.ipynb
+jupyter nbconvert --to notebook --inplace pestpp-opt.ipynb
