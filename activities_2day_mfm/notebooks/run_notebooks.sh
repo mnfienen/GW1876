5ddc4a2dce7433d68f424fb0c2e3f76be5e9f2e1
Jeremy White <jtwhite1000@gmail.com>
2019-04-28 16:05:18 +1200

    added notebook pdfs

--- activities_2day_mfm/notebooks/run_notebooks.sh
+++ activities_2day_mfm/notebooks/run_notebooks.sh
@@ -1,5 +1,10 @@
 jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace setup_pest_interface.ipynb
+jupyter nbconvert --to pdf setup_pest_interface.ipynb
 jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace prior_montecarlo.ipynb
+jupyter nbconvert --to pdf prior_montecarlo.ipynb
 jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-glm.ipynb
+jupyter nbconvert --to pdf pestpp-glm.ipynb
 jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-ies.ipynb
+jupyter nbconvert --to pdf pestpp-ies.ipynb
 jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-opt.ipynb
+jupyter nbconvert --to pdf pestpp-opt.ipynb
