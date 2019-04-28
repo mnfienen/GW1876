496d9257f0456ef9685a4a37f02828a9f224f9e0
Jeremy White <jtwhite1000@gmail.com>
2019-04-28 13:48:20 +1200

    working on the mechanics and pathing to bins :barf:

--- activities_2day_mfm/notebooks/run_notebooks.sh
+++ activities_2day_mfm/notebooks/run_notebooks.sh
@@ -1,5 +1,5 @@
-jupyter nbconvert --to notebook --inplace setup_pest_interface.ipynb
-jupyter nbconvert --to notebook --inplace prior_montecarlo.ipynb
-jupyter nbconvert --to notebook --inplace pestpp-glm.ipynb
-jupyter nbconvert --to notebook --inplace pestpp-ies.ipynb
-jupyter nbconvert --to notebook --inplace pestpp-opt.ipynb
+jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace setup_pest_interface.ipynb
+jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace prior_montecarlo.ipynb
+jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-glm.ipynb
+jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-ies.ipynb
+jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-opt.ipynb
