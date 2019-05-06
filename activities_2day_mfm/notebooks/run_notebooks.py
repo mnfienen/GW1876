cdf985c0c85449f781b24140657d42f9a50d2c4f
Jeremy White <jtwhite1000@gmail.com>
2019-05-07 11:05:59 +1200

    added optional phi balancing to prior mc notebook, reran notebooks

--- activities_2day_mfm/notebooks/run_notebooks.py
+++ activities_2day_mfm/notebooks/run_notebooks.py
@@ -2,13 +2,25 @@ import os
 
 os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace setup_pest_interface.ipynb")
 os.system("jupyter nbconvert --to pdf setup_pest_interface.ipynb")
+os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace setup_pest_interface.ipynb")
+
 os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace prior_montecarlo.ipynb")
 os.system("jupyter nbconvert --to pdf prior_montecarlo.ipynb")
+os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace prior_montecarlo.ipynb")
+
 os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-glm.ipynb")
 os.system("jupyter nbconvert --to pdf pestpp-glm.ipynb")
+os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace pest_glm.ipynb")
+
 os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-ies.ipynb")
 os.system("jupyter nbconvert --to pdf pestpp-ies.ipynb")
+os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace pestpp-ies.ipynb")
+
 os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-opt.ipynb")
 os.system("jupyter nbconvert --to pdf pestpp-opt.ipynb")
+os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace pestpp_opt.ipynb")
+
 os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-ies_the_wrong_way.ipynb")
 os.system("jupyter nbconvert --to pdf pestpp-ies_the_wrong_way.ipynb")
+os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace pestpp-ies_the_wrong_way.ipynb")
+
