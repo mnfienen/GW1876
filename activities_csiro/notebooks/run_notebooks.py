594cbb5d455a0d304d46b0f44c6ab39094a8cc3e
Jeremy White <jtwhite1000@gmail.com>
2019-07-01 13:41:16 +1200

    removed par-based scenario, reverted to rch-wel to handle scenario.  added prior cov processing to glm part 2

--- activities_csiro/notebooks/run_notebooks.py
+++ activities_csiro/notebooks/run_notebooks.py
@@ -8,9 +8,17 @@ os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inpl
 os.system("jupyter nbconvert --to pdf prior_montecarlo.ipynb")
 os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace prior_montecarlo.ipynb")
 
-os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-glm.ipynb")
-os.system("jupyter nbconvert --to pdf pestpp-glm.ipynb")
-os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace pest_glm.ipynb")
+os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-glm_part1.ipynb")
+os.system("jupyter nbconvert --to pdf pestpp-glm_part1.ipynb")
+os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace pest_glm_part1.ipynb")
+
+os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace dataworth.ipynb")
+os.system("jupyter nbconvert --to pdf dataworth.ipynb")
+os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace dataworth.ipynb")
+
+os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-glm_part2.ipynb")
+os.system("jupyter nbconvert --to pdf pestpp-glm_part2.ipynb")
+os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace pest_glm_part2.ipynb")
 
 os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-ies.ipynb")
 os.system("jupyter nbconvert --to pdf pestpp-ies.ipynb")
