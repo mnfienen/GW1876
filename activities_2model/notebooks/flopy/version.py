99224325db5c02aa5b82b759f28c834acb7198e3
White <jtwhite1000@gmail.com>
2020-02-03 10:43:13 -0700

    initial commit for 2-model notebooks, including transient setup history model

---
+++ activities_2model/notebooks/flopy/version.py
@@ -0,0 +1,7 @@
+# flopy version file automatically created using...make-release.py
+# created on...December 15, 2019 09:55:43
+
+major = 3
+minor = 3
+micro = 1
+__version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
