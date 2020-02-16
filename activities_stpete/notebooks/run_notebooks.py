9ffa2b4163db70c09aa4e23dbb5351c6cacf8484
White <jtwhite1000@gmail.com>
2020-02-15 21:50:12 -0700

    added bmw notebook,

--- activities_stpete/notebooks/run_notebooks.py
+++ activities_stpete/notebooks/run_notebooks.py
@@ -12,6 +12,19 @@ os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inpl
 os.system("jupyter nbconvert --to pdf process_obs_and_set_weights.ipynb")
 os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace process_obs_and_set_weights.ipynb")
 
+os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-ies_part1.ipynb")
+os.system("jupyter nbconvert --to pdf pestpp-ies_part1.ipynb")
+os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace pestpp-ies_part1.ipynb")
+
+os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-ies_part2.ipynb")
+os.system("jupyter nbconvert --to pdf pestpp-ies_part2.ipynb")
+os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace pestpp-ies_part2.ipynb")
+
+os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-ies_bmw.ipynb")
+os.system("jupyter nbconvert --to pdf pestpp-ies_bmw.ipynb")
+os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace pestpp-ies_bmw.ipynb")
+
+
 os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace prior_montecarlo.ipynb")
 os.system("jupyter nbconvert --to pdf prior_montecarlo.ipynb")
 os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace prior_montecarlo.ipynb")
@@ -28,13 +41,6 @@ os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inpl
 os.system("jupyter nbconvert --to pdf pestpp-glm_part2.ipynb")
 os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace pest_glm_part2.ipynb")
 
-os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-ies_part1.ipynb")
-os.system("jupyter nbconvert --to pdf pestpp-ies_part1.ipynb")
-os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace pestpp-ies_part1.ipynb")
-
-os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-ies_part2.ipynb")
-os.system("jupyter nbconvert --to pdf pestpp-ies_part2.ipynb")
-os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace pestpp-ies_part2.ipynb")
 
 os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-opt.ipynb")
 os.system("jupyter nbconvert --to pdf pestpp-opt.ipynb")
