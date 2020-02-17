52a505fe8c2da5059a3ceb2dd423f2507a80ef67
White <jtwhite1000@gmail.com>
2020-02-16 21:02:53 -0700

    more work in prior mc

--- activities_stpete/notebooks/setup_daily_truth.py
+++ activities_stpete/notebooks/setup_daily_truth.py
@@ -66,8 +66,8 @@ def run_draws_and_pick_truth(run=True):
     np.random.seed(seed=1)
     fo_obs = nz_obs.loc[nz_obs.obsnme.apply(lambda x: "fo_" in x),:].copy()
     fo_snd = np.random.randn(fo_obs.shape[0])
-    #fo_noise = fo_snd * (fo_obs.obsval * 0.2)
-    fo_noise = fo_snd * 1./fo_obs.weight
+    fo_noise = fo_snd * (fo_obs.obsval * 0.2)
+    #fo_noise = fo_snd * 1./fo_obs.weight
     pst.observation_data.loc[fo_obs.obsnme,"obsval"] += fo_noise
 
     # Just for fun, lets have some "model error"
@@ -79,7 +79,7 @@ def run_draws_and_pick_truth(run=True):
 
 
     #add a trend to the flow obs
-    trend = np.linspace(1.0,0.7,fo_obs.shape[0])
+    trend = np.linspace(1.0,0.8,fo_obs.shape[0])
     pst.observation_data.loc[fo_obs.obsnme,"obsval"] *= trend
 
     #add some "spikes"
