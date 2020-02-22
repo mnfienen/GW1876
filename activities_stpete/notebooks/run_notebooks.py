1f0ed4fe1f44090feee56f01532f82c3d91c1c9f
White <jtwhite1000@gmail.com>
2020-02-22 10:25:56 -0500

    added more verbosity:

--- activities_stpete/notebooks/run_notebooks.py
+++ activities_stpete/notebooks/run_notebooks.py
@@ -1,37 +1,30 @@
 import os
 
-os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace setup_transient_history.ipynb")
+os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --allow-errors --inplace setup_transient_history.ipynb")
 os.system("jupyter nbconvert --to pdf setup_transient_history.ipynb")
-os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace setup_transient_history.ipynb")
+os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --allow-errors --inplace setup_transient_history.ipynb")
 
-os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace setup_pest_interface.ipynb")
+os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --allow-errors --inplace setup_pest_interface.ipynb")
 os.system("jupyter nbconvert --to pdf setup_pest_interface.ipynb")
 os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace setup_pest_interface.ipynb")
 
-os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace process_obs_and_set_weights.ipynb")
+os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --allow-errors --inplace process_obs_and_set_weights.ipynb")
 os.system("jupyter nbconvert --to pdf process_obs_and_set_weights.ipynb")
 os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace process_obs_and_set_weights.ipynb")
 
-os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-ies_part1.ipynb")
+os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --allow-errors --inplace pestpp-ies_part1.ipynb")
 os.system("jupyter nbconvert --to pdf pestpp-ies_part1.ipynb")
 os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace pestpp-ies_part1.ipynb")
 
-
-
-'''
-os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-ies_part2.ipynb")
+os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --allow-errors --inplace pestpp-ies_part2.ipynb")
 os.system("jupyter nbconvert --to pdf pestpp-ies_part2.ipynb")
 os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace pestpp-ies_part2.ipynb")
 
-os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-ies_bmw.ipynb")
+os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --allow-errors --inplace pestpp-ies_bmw.ipynb")
 os.system("jupyter nbconvert --to pdf pestpp-ies_bmw.ipynb")
 os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace pestpp-ies_bmw.ipynb")
-'''
 
-
-
-
-os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace prior_montecarlo.ipynb")
+os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --allow-errors --inplace prior_montecarlo.ipynb")
 os.system("jupyter nbconvert --to pdf prior_montecarlo.ipynb")
 os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace prior_montecarlo.ipynb")
 
@@ -39,11 +32,11 @@ os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inpl
 os.system("jupyter nbconvert --to pdf pestpp-glm_part1.ipynb")
 os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace pest_glm_part1.ipynb")
 
-os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace dataworth.ipynb")
+os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --allow-errors --inplace dataworth.ipynb")
 os.system("jupyter nbconvert --to pdf dataworth.ipynb")
 os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace dataworth.ipynb")
 
-os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --inplace pestpp-glm_part2.ipynb")
+os.system("jupyter nbconvert --execute --ExecutePreprocessor.timeout=1800 --allow-errors --inplace pestpp-glm_part2.ipynb")
 os.system("jupyter nbconvert --to pdf pestpp-glm_part2.ipynb")
 os.system("jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace pest_glm_part2.ipynb")
 
